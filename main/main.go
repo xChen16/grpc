f41c139dfad2f502b7f7540ee5ab06189a78c20f
xChen16 <ares_0427@163.com>
2021-03-19 18:02:24 +0800

    client

--- main/main.go
+++ main/main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"sync"
 	"time"
 
 	"github.com/grpc"
-
-	"github.com/grpc/codec"
 )
 
 func startServer(addr chan string) {
@@ -23,26 +21,32 @@ func startServer(addr chan string) {
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
-
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	/*
+		conn, _ := net.Dial("tcp", <-addr)
+		defer func() { _ = conn.Close() }()
+	*/
+	client, _ := grpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 	time.Sleep(time.Second)
 	// send options
-	_ = json.NewEncoder(conn).Encode(grpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
+	/* _ = json.NewEncoder(conn).Encode(grpc.DefaultOption)
+	cc := codec.NewGobCodec(conn) */
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
-
+	wg.Wait()
 }
